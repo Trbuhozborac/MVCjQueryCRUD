4859069993d928681f4bbdc6b059a50520ea2c87
pomorange <nemanjapipi@hotmail.com>
2019-07-01 00:49:00 +0200

    finish preparation for crud operations

---
+++ jQueryAjaxCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using jQueryAjaxCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryAjaxCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployees());
+        }
+
+        IEnumerable<Employee> GetAllEmployees()
+        {
+            using(DbModel db = new DbModel())
+            {
+                return db.Employee.ToList<Employee>();
+            }
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
